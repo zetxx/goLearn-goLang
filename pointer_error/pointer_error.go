28d9b79aea1f42131081225e6b7b7456eac97793
zetxx <zetxx@belogradchik.biz>
2024-08-20 15:23:35 +0300

    next

--- pointer_error/pointer_error.go
+++ pointer_error/pointer_error.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type (
+	Bitcoin int64
+	Wallet  struct {
+		balance Bitcoin
+	}
+	Stringer interface {
+		String() string
+	}
+)
+
+var lowBalance = errors.New("LowBalance")
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(ammount Bitcoin) {
+	w.balance = w.balance + ammount
+}
+
+func (w *Wallet) Withdraw(ammount Bitcoin) error {
+	if w.balance < ammount {
+		return lowBalance
+	}
+	w.balance = w.balance - ammount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
--- pointer_error/pointer_error_test.go
+++ pointer_error/pointer_error_test.go
@@ -1,1 +1,40 @@
 package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s != want %s", got, want)
+		}
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if err != nil {
+			t.Errorf("error returned but it should not!")
+		}
+		if got != want {
+			t.Errorf("got %s != want %s", got, want)
+		}
+	})
+	t.Run("Withdraw more thant we have in deposit", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		if err == nil {
+			t.Error("Wanted error but got nil")
+		}
+		if err.Error() != lowBalance.Error() {
+			t.Errorf("Inncorect error, want: %s, got: %s", lowBalance.Error(), err.Error())
+		}
+	})
+}
