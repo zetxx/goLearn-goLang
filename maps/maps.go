6baf4f392f9263159582ca5e442cf8e7c8d6d009
Elin Angelow <elin.angelov@shelly.com>
2024-08-22 11:30:48 +0300

    fin

--- maps/maps.go
+++ maps/maps.go
@@ -1,1 +1,50 @@
 package main
+
+type Dict map[string]string
+
+const (
+	ErrorWordNotFound = DictError("WordNotFound")
+	ErrorWordFound    = DictError("WordFound")
+)
+
+type DictError string
+
+func (e DictError) Error() string {
+	return string(e)
+}
+
+func (dict Dict) Search(word string) (string, error) {
+	w, f := dict[word]
+	if f == false {
+		return "", ErrorWordNotFound
+	}
+	return w, nil
+}
+
+func (dict Dict) Add(key, value string) error {
+	_, err := dict.Search(key)
+
+	if err == nil {
+		return ErrorWordFound
+	}
+	dict[key] = value
+	return nil
+}
+
+func (dict Dict) Update(key, newVal string) error {
+	_, e := dict.Search(key)
+	if e != nil {
+		return e
+	}
+	dict[key] = newVal
+	return nil
+}
+
+func (dict Dict) Delete(key string) error {
+	_, e := dict.Search(key)
+	if e != nil {
+		return e
+	}
+	delete(dict, key)
+	return nil
+}
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -1,1 +1,106 @@
-package maps
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	t.Run("Search", func(t *testing.T) {
+		dict := Dict{"test": "this is just a test"}
+		got, e := dict.Search("test")
+		want := "this is just a test"
+
+		if e != nil {
+			t.Errorf("Error returned, but should not")
+		}
+		if got != want {
+			t.Errorf("got %s != want %s", got, want)
+		}
+	})
+	t.Run("Search, word not found", func(t *testing.T) {
+		dict := Dict{"test": "this is just a test"}
+		_, e := dict.Search("test_")
+
+		if e == nil {
+			t.Errorf("Error not returned, but should")
+		}
+		if e.Error() != ErrorWordNotFound.Error() {
+			t.Errorf("got %s != want %s", e.Error(), ErrorWordNotFound.Error())
+		}
+	})
+	t.Run("Add", func(t *testing.T) {
+		dict := Dict{"test": "this is just a test"}
+		e := dict.Add("aa", "aaa")
+		if e != nil {
+			t.Errorf("Error returned, word exits, but should not")
+		}
+		got, e := dict.Search("aa")
+		want := "aaa"
+
+		if e != nil {
+			t.Errorf("Error returned, but should not")
+		}
+		if got != want {
+			t.Errorf("got %s != want %s", got, want)
+		}
+		e2 := dict.Add("aa", "aaa")
+		if e2 == nil {
+			t.Errorf("Error should be returned")
+		}
+		if e2.Error() != ErrorWordFound.Error() {
+			t.Errorf("got %s != want %s", e2.Error(), ErrorWordFound.Error())
+		}
+	})
+	t.Run("Update", func(t *testing.T) {
+		dict := Dict{}
+		want := "321"
+		e := dict.Add("test", "123")
+		if e != nil {
+			t.Errorf("Error(%s) returned, but should not: Add", e.Error())
+		}
+		e2 := dict.Update("test", want)
+		if e2 != nil {
+			t.Errorf("Error(%s) returned, but should not: Update", e2.Error())
+		}
+		got, e3 := dict.Search("test")
+		if e3 != nil {
+			t.Errorf("Error(%s) returned, but should not: Search", e3.Error())
+		}
+		if got != want {
+			t.Errorf("Got %s != want %s", got, want)
+		}
+	})
+	t.Run("Update, word not found", func(t *testing.T) {
+		dict := Dict{}
+		e2 := dict.Update("test", "123")
+		if e2 == nil {
+			t.Errorf("Error not returned, but should : Update")
+		}
+		if e2.Error() != ErrorWordNotFound.Error() {
+			t.Errorf("got %s != want %s", e2.Error(), ErrorWordNotFound.Error())
+		}
+	})
+	t.Run("Delete", func(t *testing.T) {
+		dict := Dict{}
+		e := dict.Add("test", "123")
+		if e != nil {
+			t.Errorf("Error(%s) returned, but should not: Add", e.Error())
+		}
+		e2 := dict.Delete("test")
+		if e2 != nil {
+			t.Errorf("Error(%s) returned, but should not: Delete", e2.Error())
+		}
+		_, e3 := dict.Search("test")
+		if e3 == nil {
+			t.Errorf("Error not returned, but should: Search")
+		}
+	})
+	t.Run("Delete, word not found", func(t *testing.T) {
+		dict := Dict{}
+		e2 := dict.Delete("test")
+		if e2 == nil {
+			t.Errorf("Error not returned, but should : Update")
+		}
+		if e2.Error() != ErrorWordNotFound.Error() {
+			t.Errorf("got %s != want %s", e2.Error(), ErrorWordNotFound.Error())
+		}
+	})
+}
