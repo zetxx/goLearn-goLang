479c66faac579675105bc790e5fa37d55e2fd4d9
Elin Angelow <elin.angelov@shelly.com>
2024-09-10 18:11:29 +0300

    http test

--- http_test/http.go
+++ http_test/http.go
@@ -1,1 +1,27 @@
 package httptest
+
+import (
+	"errors"
+	"net/http"
+	"time"
+)
+
+func Race(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", errors.New("slow response")
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
--- http_test/http_test.go
+++ http_test/http_test.go
@@ -1,1 +1,45 @@
 package httptest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func spawnServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+
+func TestRace(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		slowServer := spawnServer(2 * time.Second)
+		fastServer := spawnServer(0 * time.Second)
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
+		want := fastUrl
+
+		got, _ := Race(slowUrl, fastUrl, 5*time.Second)
+
+		if got != want {
+			t.Errorf("got %s != want %s", got, want)
+		}
+	})
+	t.Run("2", func(t *testing.T) {
+		slowServer := spawnServer(4 * time.Second)
+		fastServer := spawnServer(0 * time.Second)
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		_, err := Race(slowServer.URL, fastServer.URL, 3*time.Second)
+		if err != nil {
+			t.Errorf("want error, didnt get error")
+		}
+	})
+}
